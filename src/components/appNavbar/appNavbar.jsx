0682afb8498e98c514730d8598e2cfc2ffbafffd
Mindaugas Kapickas <mindaugas@mkp.com>
2021-06-18 09:53:21 +0300

    css flex for navbar

--- src/components/appNavbar/appNavbar.jsx
+++ src/components/appNavbar/appNavbar.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
+import './style.css'
 class AppNavbar extends Component {
     state = {  }
     render() { 
         return ( <header className='navbar'>
-            <i class="fa fa-fighter-jet" to='/'></i>
-            <input type="text" />
+            <a href="/"><strong>Keep</strong>Clone</a>
+            <div className='search'>
+                <input type="search" placeholder='search'/>
+            </div>
+            <div>
+                <i className='fa fa-pencil'></i>
+            </div>
         </header> ); 
     }
 }
